c9065ec066d3be576221124575192b0f9826086e
Krishna Patel <>
2019-09-07 12:14:00 -0400

    components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,8 @@ import { areAllEquivalent } from '@angular/compiler/src/output/output_ast';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //styleUrls: ['./app.component.css']
-  styles: [`
-  h3 {
-    color: aqua;
-  }
-  `]
+  styleUrls: ['./app.component.css']
+  
 })
 export class AppComponent {
   name = 'Krishna';
